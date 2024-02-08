535ea3b27d2179d23558e7a2572ac298c24c538f
Joseph Segbefia <elorma.segbefia@gmail.com>
2024-02-08 23:07:00 +0000

    Start service file

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const SignUp = () => {
   return (
