11c2f2a2335461e79db7a9b239d683498d768d90
Joseph Segbefia <elorma.segbefia@gmail.com>
2024-02-14 21:06:06 +0000

    Start CreatePortfolio.jsx

---
+++ src/components/portfolio/CreatePortfolio.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import React, { useEffect } from 'react';
+
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+const CreatePortfolio = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default CreatePortfolio
