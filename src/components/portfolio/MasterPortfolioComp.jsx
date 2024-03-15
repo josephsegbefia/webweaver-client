3e25b018cfd6595da5eab5554657f9019605aacc
Joseph Segbefia <elorma.segbefia@gmail.com>
2024-03-15 16:15:58 +0100

    Fetch projects from backend

--- src/components/portfolio/MasterPortfolioComp.jsx
+++ src/components/portfolio/MasterPortfolioComp.jsx
@@ -12,80 +12,97 @@ import { AuthContext } from '../../context/auth.context';
 
 const API_URL = import.meta.env.VITE_API_URL;
 
-const projects = [
-  {
-    title: "First Project",
-    description: "A first description",
-    imgUrl: "https://res.cloudinary.com/dexb8osc9/image/upload/v1710262854/webweaver/innjvjnqhkaizgo4nzoa.jpg",
-    shortDesc: "A short description",
-    techsUsed: ["react", "HTML", "CSS"]
-  },
-  {
-    title: "Second Project",
-    description: "A second description",
-    imgUrl: "https://res.cloudinary.com/dexb8osc9/image/upload/v1710262854/webweaver/innjvjnqhkaizgo4nzoa.jpg",
-    shortDesc: "A short description",
-    techsUsed: ["react", "HTML", "CSS"]
-  },
-  {
-    title: "First Project",
-    description: "A first description",
-    imgUrl: "https://res.cloudinary.com/dexb8osc9/image/upload/v1710262854/webweaver/innjvjnqhkaizgo4nzoa.jpg",
-    shortDesc: "A short description",
-    techsUsed: ["react", "HTML", "CSS"]
-  },
-  {
-    title: "First Project",
-    description: "A first description",
-    imgUrl: "https://res.cloudinary.com/dexb8osc9/image/upload/v1710262854/webweaver/innjvjnqhkaizgo4nzoa.jpg",
-    shortDesc: "A short description",
-    techsUsed: ["react", "HTML", "CSS"]
-  },
-  {
-    title: "Second Project",
-    description: "A second description",
-    imgUrl: "https://res.cloudinary.com/dexb8osc9/image/upload/v1710262854/webweaver/innjvjnqhkaizgo4nzoa.jpg",
-    shortDesc: "A short description",
-    techsUsed: ["react", "HTML", "CSS"]
-  },
-  {
-    title: "First Project",
-    description: "A first description",
-    imgUrl: "https://res.cloudinary.com/dexb8osc9/image/upload/v1710262854/webweaver/innjvjnqhkaizgo4nzoa.jpg",
-    shortDesc: "A short description",
-    techsUsed: ["react", "HTML", "CSS"]
-  },
-  {
-    title: "First Project",
-    description: "A first description",
-    imgUrl: "https://res.cloudinary.com/dexb8osc9/image/upload/v1710262854/webweaver/innjvjnqhkaizgo4nzoa.jpg",
-    shortDesc: "A short description",
-    techsUsed: ["react", "HTML", "CSS"]
-  },
-  {
-    title: "Second Project",
-    description: "A second description",
-    imgUrl: "https://res.cloudinary.com/dexb8osc9/image/upload/v1710262854/webweaver/innjvjnqhkaizgo4nzoa.jpg",
-    shortDesc: "A short description",
-    techsUsed: ["react", "HTML", "CSS"]
-  },
-  {
-    title: "First Project",
-    description: "A first description",
-    imgUrl: "https://res.cloudinary.com/dexb8osc9/image/upload/v1710262854/webweaver/innjvjnqhkaizgo4nzoa.jpg",
-    shortDesc: "A short description",
-    techsUsed: ["react", "HTML", "CSS"]
-  }
-]
+// const projects = [
+//   {
+//     title: "First Project",
+//     description: "A first description",
+//     imgUrl: "https://res.cloudinary.com/dexb8osc9/image/upload/v1710262854/webweaver/innjvjnqhkaizgo4nzoa.jpg",
+//     shortDesc: "A short description",
+//     techsUsed: ["react", "HTML", "CSS"]
+//   },
+//   {
+//     title: "Second Project",
+//     description: "A second description",
+//     imgUrl: "https://res.cloudinary.com/dexb8osc9/image/upload/v1710262854/webweaver/innjvjnqhkaizgo4nzoa.jpg",
+//     shortDesc: "A short description",
+//     techsUsed: ["react", "HTML", "CSS"]
+//   },
+//   {
+//     title: "First Project",
+//     description: "A first description",
+//     imgUrl: "https://res.cloudinary.com/dexb8osc9/image/upload/v1710262854/webweaver/innjvjnqhkaizgo4nzoa.jpg",
+//     shortDesc: "A short description",
+//     techsUsed: ["react", "HTML", "CSS"]
+//   },
+//   {
+//     title: "First Project",
+//     description: "A first description",
+//     imgUrl: "https://res.cloudinary.com/dexb8osc9/image/upload/v1710262854/webweaver/innjvjnqhkaizgo4nzoa.jpg",
+//     shortDesc: "A short description",
+//     techsUsed: ["react", "HTML", "CSS"]
+//   },
+//   {
+//     title: "Second Project",
+//     description: "A second description",
+//     imgUrl: "https://res.cloudinary.com/dexb8osc9/image/upload/v1710262854/webweaver/innjvjnqhkaizgo4nzoa.jpg",
+//     shortDesc: "A short description",
+//     techsUsed: ["react", "HTML", "CSS"]
+//   },
+//   {
+//     title: "First Project",
+//     description: "A first description",
+//     imgUrl: "https://res.cloudinary.com/dexb8osc9/image/upload/v1710262854/webweaver/innjvjnqhkaizgo4nzoa.jpg",
+//     shortDesc: "A short description",
+//     techsUsed: ["react", "HTML", "CSS"]
+//   },
+//   {
+//     title: "First Project",
+//     description: "A first description",
+//     imgUrl: "https://res.cloudinary.com/dexb8osc9/image/upload/v1710262854/webweaver/innjvjnqhkaizgo4nzoa.jpg",
+//     shortDesc: "A short description",
+//     techsUsed: ["react", "HTML", "CSS"]
+//   },
+//   {
+//     title: "Second Project",
+//     description: "A second description",
+//     imgUrl: "https://res.cloudinary.com/dexb8osc9/image/upload/v1710262854/webweaver/innjvjnqhkaizgo4nzoa.jpg",
+//     shortDesc: "A short description",
+//     techsUsed: ["react", "HTML", "CSS"]
+//   },
+//   {
+//     title: "First Project",
+//     description: "A first description",
+//     imgUrl: "https://res.cloudinary.com/dexb8osc9/image/upload/v1710262854/webweaver/innjvjnqhkaizgo4nzoa.jpg",
+//     shortDesc: "A short description",
+//     techsUsed: ["react", "HTML", "CSS"]
+//   }
+// ]
 
 const MasterPortfolioComp = () => {
   const [portfolioOwner, setPortfolioOwner] = useState(false);
+  const [projects, setProjects] = useState([]);
   const [editMode, setEditMode] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState()
 
+  const limit = 6;
   const { user, isLoggedIn } = useContext(AuthContext);
 
   const { uniqueIdentifier } = useParams();
 
+  const fetchProjects = async () => {
+    try {
+      const response = await axios.get(`${API_URL}api/portfolios/${uniqueIdentifier}/projects?limit=${limit}&offset=${(currentPage - 1) * limit }`);
+      setProjects(response.data);
+      console.log(response.data);
+    }catch(error){
+      console.log("Error fetching projects", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchProjects();
+  }, [currentPage]);
 
 
 
