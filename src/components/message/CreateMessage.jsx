d33bff945c60d8dbe60bc8eee67f4809dec70862
Joseph Segbefia <elorma.segbefia@gmail.com>
2024-03-21 21:05:58 +0100

    Message sending feature done

--- src/components/message/CreateMessage.jsx
+++ src/components/message/CreateMessage.jsx
@@ -1,5 +1,7 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react'
+import React, { useState, useContext, useEffect } from 'react';
+import { AuthContext } from '../../context/auth.context';
+import axios from "axios";
 
 const CreateMessage = () => {
   const [fullName, setFullName] = useState("");
@@ -7,8 +9,35 @@ const CreateMessage = () => {
   const [emailAddress, setEmailAddress] = useState("")
   const [message, setMessage] = useState("");
   const [loading, setLoading] = useState(false);
+  const [successMessage, setSuccessMessage] = useState(undefined);
+  const [errorMessage, setErrorMessage] = useState(undefined);
+  const [reload, setReload] = useState(false);
 
   const API_URL = import.meta.env.VITE_API_URL;
+
+
+
+  const { user } = useContext(AuthContext);
+
+  let uniqueIdentifier;
+  user && (uniqueIdentifier = user.uniqueIdentifier);
+
+  const cancel  = () => {
+    setFullName("");
+    setMessage("");
+    setEmailAddress("");
+    setMessageSubject("");
+  }
+
+  useEffect(() => {
+    // Empty
+  }, [reload]);
+
+  const reloadPage = () => {
+    setReload(reload => !reload);
+    setErrorMessage(undefined);
+    setSuccessMessage(undefined);
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -20,6 +49,21 @@ const CreateMessage = () => {
       senderEmail: emailAddress,
       content: message
     }
+    axios.post(`${API_URL}api/portfolios/${uniqueIdentifier}/messages`, requestBody)
+      .then((response) => {
+        // console.log(response);
+        setLoading(false);
+        setFullName("");
+        setMessage("");
+        setEmailAddress("");
+        setMessageSubject("");
+        setSuccessMessage(`Message sent! ${response.data.message}`)
+      })
+      .catch((error) => {
+        console.log(error)
+        setLoading(false);
+        setErrorMessage(error.response.data.message)
+      })
   }
   return (
     <div className = "container">
@@ -27,6 +71,33 @@ const CreateMessage = () => {
       <hr />
       <div className = "columns">
         <div className="column is-half is-offset-one-quarter">
+          {loading && (
+            <progress className = 'progress is-medium is-link' max = '100'>
+              60%
+            </progress>
+          )}
+          {successMessage && (
+            <article className="message is-success">
+              <div className="message-header">
+                <p>Success</p>
+                <button onClick = {reloadPage} className="delete" aria-label="delete"></button>
+              </div>
+              <div className = "message-body">
+                {successMessage}
+              </div>
+            </article>
+          )}
+          {errorMessage && (
+            <article className="message is-danger">
+              <div className="message-header">
+                <p>Error</p>
+                <button onClick = {reloadPage} className="delete" aria-label="delete"></button>
+              </div>
+              <div className = "message-body">
+                {errorMessage}
+              </div>
+            </article>
+          )}
           <form onSubmit = {handleSubmit}>
             <div className="card">
               <div className="card-content">
@@ -40,6 +111,7 @@ const CreateMessage = () => {
                           placeholder="Your full name"
                           value={fullName}
                           onChange={(e) => setFullName(e.target.value)}
+                          required
                         />
                       </p>
                     </div>
@@ -55,6 +127,23 @@ const CreateMessage = () => {
                           placeholder="Subject of your message"
                           value={messageSubject}
                           onChange={(e) => setMessageSubject(e.target.value)}
+                          required
+                        />
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className = "columns">
+                  <div className = "column">
+                    <div className="field">
+                      <p className="control">
+                        <input
+                          className="input message-input has-text-success"
+                          type="email"
+                          placeholder="Email Address"
+                          value={emailAddress}
+                          onChange={(e) => setEmailAddress(e.target.value)}
+                          required
                         />
                       </p>
                     </div>
@@ -68,6 +157,7 @@ const CreateMessage = () => {
                           className="textarea message-input has-text-success"
                           type="text"
                           placeholder="Your message"
+                          rows="2"
                           value={message}
                           onChange={(e) => setMessage(e.target.value)}
                         />
@@ -78,13 +168,13 @@ const CreateMessage = () => {
               </div>
               <footer className = "card-footer">
                 <p className="card-footer-item">
-                  <span className = "message-icon-cancel">
+                  <span className = "message-icon-cancel" onClick = {cancel}>
                   <i className = "fa-solid fa-xmark"></i>
                   </span>
                 </p>
                 <p className="card-footer-item">
-                  <span className = "message-icon">
-                  <i className = "fa-solid fa-paper-plane"></i>
+                  <span>
+                  <button type = "submit" className = "submit-button"><i className = "fa-solid fa-paper-plane"></i></button>
                   </span>
                 </p>
               </footer>
