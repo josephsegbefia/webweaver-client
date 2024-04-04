feaa047317eabbf39faf160d549b58c3de1fb5b6
Joseph Segbefia <elorma.segbefia@gmail.com>
2024-04-04 23:42:46 +0200

    Create IsAdmin Component

---
+++ src/components/auth/IsAdmin.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import { useContext } from 'react';
+import { AuthContext } from '../../context/auth.context';
+import { Navigate } from 'react-router-dom';
+
+function IsAdmin({ children }) {
+  const { isLoggedIn, user } = useContext(AuthContext);
+
+  if(!isLoggedIn && !user.isAdmin){
+    return <Navigate to = '/' />
+  } else {
+    return children;
+  }
+}
+
+export default IsAdmin;
